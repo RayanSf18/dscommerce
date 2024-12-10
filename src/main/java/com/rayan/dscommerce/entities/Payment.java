cecfea3320d3e8c49b7a0d6874ac640afb7cc8fa
Rayan Sf <rayansf18@gmail.com>
2024-12-10 16:21:26 -0300

    Payment class mapped

--- src/main/java/com/rayan/dscommerce/entities/Payment.java
+++ src/main/java/com/rayan/dscommerce/entities/Payment.java
@@ -1,12 +1,25 @@
 package com.rayan.dscommerce.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.time.Instant;
 
+@Entity
+@Table(name = "tb_payment")
 public class Payment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
+
 
     private Order order;
 
