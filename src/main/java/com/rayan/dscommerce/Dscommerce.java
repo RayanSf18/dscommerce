de5dcdb4b54d22262e0d5ecbd823e9504b03a52b
Rayan Sf <rayansf18@gmail.com>
2024-12-10 11:51:02 -0300

    project created

---
+++ src/main/java/com/rayan/dscommerce/Dscommerce.java
@@ -0,0 +1,12 @@
+package com.rayan.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscommerce {
+	public static void main(String[] args) {
+		SpringApplication.run(Dscommerce.class, args);
+	}
+
+}
