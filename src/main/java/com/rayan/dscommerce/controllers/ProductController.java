fcdf462ad1f1aa61026e62d01399831eae529692
Rayan Sf <rayansf18@gmail.com>
2024-12-12 08:57:29 -0300

    Standardization of endpoint responses

--- src/main/java/com/rayan/dscommerce/controllers/ProductController.java
+++ src/main/java/com/rayan/dscommerce/controllers/ProductController.java
@@ -5,12 +5,16 @@ import com.rayan.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/api/v1/products")
@@ -20,17 +24,19 @@ public class ProductController {
     public ProductService productService;
 
     @PostMapping
-    public ProductDTO saveProduct(@RequestBody ProductDTO productDTO) {
-        return this.productService.saveProduct(productDTO);
+    public ResponseEntity<ProductDTO> saveProduct(@RequestBody ProductDTO productDTO) {
+        ProductDTO dto = this.productService.saveProduct(productDTO);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(location).body(dto);
     }
 
     @GetMapping(value = "/{productId}")
-    public ProductDTO searchProductById(@PathVariable(value = "productId") Long productId) {
-        return this.productService.searchProductById(productId);
+    public ResponseEntity<ProductDTO> searchProductById(@PathVariable(value = "productId") Long productId) {
+        return ResponseEntity.ok().body(this.productService.searchProductById(productId));
     }
 
     @GetMapping
-    public Page<ProductDTO> searchProducts(Pageable pageable) {
-        return this.productService.searchProducts(pageable);
+    public ResponseEntity<Page<ProductDTO>> searchProducts(Pageable pageable) {
+        return ResponseEntity.ok().body(this.productService.searchProducts(pageable));
     }
 }
