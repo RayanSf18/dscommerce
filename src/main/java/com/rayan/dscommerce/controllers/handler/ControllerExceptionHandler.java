30e58c47e67cb8e4451c9069545b141953a127cb
Rayan Silva <rayansf18@gmail.com>
2024-12-27 15:54:35 -0300

    Controller advice class

---
+++ src/main/java/com/rayan/dscommerce/controllers/handler/ControllerExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.rayan.dscommerce.controllers.handler;
+
+import com.rayan.dscommerce.dtos.CustomError;
+import com.rayan.dscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+
+}
